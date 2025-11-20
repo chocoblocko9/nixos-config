fb6bb3316bc20ef3d096c2457255ce4065aa22f0
Conor <conorboyle07@protonmail.com>
2025-11-20 11:24:51 +0000

    Made hyprland much more usable part 3

--- home.nix
+++ home.nix
@@ -83,6 +83,11 @@
         "hyprpaper"
       ];
       
+      "dwindle" = {
+        "pseudotile" = true; 
+        "preserve_split" = true; 
+      };
+
       "input" = {
         "kb_layout" = "de";
         "kb_variant" = "qwerty";
@@ -141,6 +146,44 @@
           "$mod, D, exec, $menu"
           "$mod, P, pseudo," # dwindle
           "$mod, J, togglesplit," # dwindle
+
+          # Shift focus with arrow keys
+          "$mod, left, movefocus, l"
+          "$mod, down, movefocus, d"
+          "$mod, up, movefocus, u"
+          "$mod, right, movefocus, r"
+
+          # Workspace switching with $mod + number
+          "$mod, 1, workspace, 1"
+          "$mod, 2, workspace, 2"
+          "$mod, 3, workspace, 3"
+          "$mod, 4, workspace, 4"
+          "$mod, 5, workspace, 5"
+          "$mod, 6, workspace, 6"
+          "$mod, 7, workspace, 7"
+          "$mod, 8, workspace, 8"
+          "$mod, 9, workspace, 9"
+          "$mod, 0, workspace, 10"
+
+          # Move focused window to workspace with $mod + SHIFT + number
+          "$mod SHIFT, 1, movetoworkspace, 1"
+          "$mod SHIFT, 2, movetoworkspace, 2"
+          "$mod SHIFT, 3, movetoworkspace, 3"
+          "$mod SHIFT, 4, movetoworkspace, 4"
+          "$mod SHIFT, 5, movetoworkspace, 5"
+          "$mod SHIFT, 6, movetoworkspace, 6"
+          "$mod SHIFT, 7, movetoworkspace, 7"
+          "$mod SHIFT, 8, movetoworkspace, 8"
+          "$mod SHIFT, 9, movetoworkspace, 9"
+          "$mod SHIFT, 0, movetoworkspace, 10"
+
+          # Special workspace (from example config currently)
+          "$mod, S, togglespecialworkspace, magic"
+          "$mod SHIFT, S, movetoworkspace, special:magic"
+
+          # Scroll through workspaces
+          "$mod, mouse_up, workspace, e-1"
+          "$mod, mouse_down, workspace, e+1"
       ];
     };
   };
