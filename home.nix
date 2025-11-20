0d7177a0fc74dbc09e7c8c2008565a7b496ec7f0
Conor <conorboyle07@protonmail.com>
2025-11-20 12:11:22 +0000

    Better home.packages formatting

--- home.nix
+++ home.nix
@@ -21,14 +21,14 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    pkgs.hello
-    pkgs.firefox
-    pkgs.pavucontrol
-    pkgs.zip
-    pkgs.unzip
+    hello
+    firefox
+    pavucontrol
+    zip
+    unzip
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
