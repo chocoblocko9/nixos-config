79ab2f9f7204d7dbdb417a936f963a4b7b43384e
Conor <conorbhoyle@gmail.com>
2025-11-21 21:46:09 +0000

    Wallpapers

--- hypr/hyprpaper.nix
+++ hypr/hyprpaper.nix
@@ -8,11 +8,13 @@
       splash = true;
       splash_offset = 2.0;
 
-      preload =
-        [ "/home/conor/.files/hypr/wallpapers/example.png" ];
+      preload = [
+        "/home/conor/.files/hypr/wallpapers/wallpaper.jpg"
+        "/home/conor/.files/hypr/wallpapers/wallpaper2.png"
+      ];
 
       wallpaper = 
-        [ ", /home/conor/.files/hypr/wallpapers/example.png" ];
+        [ ", /home/conor/.files/hypr/wallpapers/wallpaper.jpg" ];
     };
   };
 }
