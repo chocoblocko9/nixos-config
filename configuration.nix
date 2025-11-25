be11f36c7dff86452886d35df3c3703fd77a5bfe
Conor <conorbhoyle@gmail.com>
2025-11-25 20:57:37 +0000

    configuration.nix refactoring and fstab mounts now bound to UUID cus they keep switching around

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -13,8 +9,9 @@
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
+  # Bootloader
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.supportedFilesystems = ["ntfs"];
   boot.loader = {
     systemd-boot.enable = false;
@@ -34,7 +31,7 @@
         echo "System shutting down..."
         halt
       }
-      menuentry 'Arch Linux (on /dev/sda5)' --class arch --class gnu-linux --class gnu --class os $menuentry_id_option 'osprober-gnulinux-simple-ec21-0eb4f241-ec21-4840-8321-4c66a2f2cd89' {
+      menuentry 'Arch Linux (on 2TB Hard Drive)' --class arch --class gnu-linux --class gnu --class os $menuentry_id_option 'osprober-gnulinux-simple-ec21-0eb4f241-ec21-4840-8321-4c66a2f2cd89' {
 	      ismod part_msdos
 	      ismod fat
 	      set root='hd0,msdos3'
@@ -56,13 +53,13 @@
   
   # fstab mounts
   fileSystems."/home/conor/2TB-Hard-Drive" = {
-    device = "/dev/sdb2";
+    device = "/dev/disk/by-uuid/203EA3F63EA3C2E0";
     fsType = "ntfs";
     options = [ "users" "nofail" "exec" ];
   };
 
   fileSystems."/home/conor/1TB-Hard-Drive" = {
-    device = "/dev/sda2";
+    device = "/dev/disk/by-uuid/98046F01046EE22C";
     fsType = "ntfs";
     options = [ "users" "nofail" "exec" ];
   };
@@ -73,25 +70,12 @@
     options = [ "users" "nofail" ];
   };
 
-  # Use latest kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  
-  # Enable i2c protocol for use with ddcutil (brightness changing)
-  hardware.i2c.enable = true;
-
+  # Networking
   networking.hostName = "nixos";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
-
-  # Set your time zone.
+ 
+  # Locales
   time.timeZone = "Europe/Dublin";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_IE.UTF-8";
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "en_IE.UTF-8";
@@ -105,14 +89,35 @@
     LC_TIME = "en_IE.UTF-8";
   };
 
-  # Configure keymap in X11
+  # "hardware" options
+  hardware = {
+    i2c.enable = true; # Enable i2c protocol for use with ddcutil (brightness changing)
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+    amdgpu = {
+      opencl.enable = true;
+      overdrive = { # GPU overclocking stuff
+        enable = true;
+        ppfeaturemask = "0xfffd7fff";
+      };
+    };
+  };  
+
+  # 
+  services.lact.enable = true;
+
+  # Configure keyboard layout
+  console.useXkbConfig = true;
   services.xserver.xkb = {
     layout = "de";
     variant = "qwerty";
   };
-
-  # Configure console keymap
-  console.useXkbConfig = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.conor = {
@@ -135,47 +140,37 @@
     cava # HAS to be here for the rmpc visualiser to work, the home-manager module does not work for unknown reasons
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs = {
+    steam.enable = true;
+    thunar = {
+      enable = true;
+      plugins = with pkgs.xfce; [ thunar-volman thunar-media-tags-plugin thunar-archive-plugin thunar-vcs-plugin ];
+    };
+    hyprland = {
+      enable = true;
+      withUWSM = true;
+      xwayland.enable = true;
+    };
+  };
 
-  programs.steam.enable = true;
-
- 
-  # Hyprland 
-  programs.hyprland = {
-    enable = true;
-    withUWSM = true;
-    xwayland.enable = true;
-  };  
-
+  # Is this necessary? idk
   environment.sessionVariables.NIXOS_OZONE_WL = "1"; 
 
-  # Audio & Bluetooth
-  services.pulseaudio.enable = false;
+  # Audio
   security.rtkit.enable = true;
-   
-  services.pipewire = {
-    enable = true;
-    alsa = {
+  services = {
+    pulseaudio.enable = false;
+    pipewire = {
+      pulse.enable = true;
       enable = true;
-      support32Bit = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
     };
-    pulse.enable = true;
   };
-
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-  };
-
-
+   
   # List services that you want to enable:
-  services.samba.enable = true;
   services.displayManager.ly = {
     enable = true;
     settings = {
@@ -214,14 +209,9 @@
       clock = "%H:%M:%S %a, %d/%m/%Y";  
     };
   };
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
