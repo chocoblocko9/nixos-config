50a8e7240e39b3da473f92a7fac6ec95263d0bee
Conor <conorboyle07@protonmail.com>
2026-01-30 09:07:06 +0000

    Clean up profiles/README.md and home.nix

--- profiles/Slip/home.nix
+++ profiles/Slip/home.nix
@@ -8,23 +8,6 @@
     stateVersion = "25.11";
   };
 
-  # The home.packages option allows you to install Nix packages into your environment.
-  nixpkgs.config.allowUnfree = true;
-  home.packages = with pkgs; [
-    # Programs
-    vlc
-
-    # Tools
-    fastfetch
-    zip
-    unzip
-    feh # GUI image viewer
-    xarchiver # GUI archive manager
-    ncdu 
-    wev 
-    unipicker
-  ];
-
   home.sessionVariables = {
     # EDITOR = "emacs";
     NIXOS_OZONE_WL = "1"; 
