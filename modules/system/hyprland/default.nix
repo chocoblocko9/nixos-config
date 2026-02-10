be5c37a5d9b8a6bf20e9671bcc2d43547f1a9cbc
Conor <conorbhoyle@gmail.com>
2026-02-10 18:03:59 +0000

    Cleanup modules/system/hyprland/default.nix

--- modules/system/hyprland/default.nix
+++ modules/system/hyprland/default.nix
@@ -1,29 +1,22 @@
 { lib, config, pkgs, inputs, ... }:
 
-{
+let
+  source = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system};
+  cfg = config.systemSettings.hyprland;
+in {
   options = {
     systemSettings.hyprland = {
       enable = lib.mkEnableOption "Enable hyprland";
     };
   };
 
-  config = lib.mkIf config.systemSettings.hyprland.enable {
-    /*
-    nix.settings = {
-      substituters = ["https://hyprland.cachix.org"];
-      trusted-substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-    };
-    */
-
+  config = lib.mkIf cfg.enable {
     programs.hyprland = {
       enable = true;
       withUWSM = true;
       xwayland.enable = true;
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland.overrideAttrs {
-        patches = [ ./../../../overlays/temp_fix_hyprland.patch ];
-      };
-      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      package = source.hyprland;
+      portalPackage = source.xdg-desktop-portal-hyprland;
     };
   };
 }
