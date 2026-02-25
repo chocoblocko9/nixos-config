5982bc133d02dd98b91f570ec4d937045c36e844
Conor <conorbhoyle@gmail.com>
2026-02-25 19:17:01 +0000

    Switched to btrfs cus why not, it's basically just better (hah)

--- profiles/slip/hardware-configuration.nix
+++ profiles/slip/hardware-configuration.nix
@@ -9,11 +9,18 @@
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.extraModulePackages = [ ];
+  boot.extraModulePackages = [ ]; 
 
+  /* 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/cbdff699-e708-4049-9515-9e66e16ff2b3";
       fsType = "ext4";
+    };
+  */
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/ca7a632a-4555-49ee-b7d4-461010879116";
+      fsType = "btrfs";
     };
 
   fileSystems."/boot" =
