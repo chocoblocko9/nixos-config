88230802a9f1e7bffd1edcb1e9ab2836f74dff58
Conor <conorbhoyle@gmail.com>
2026-03-04 07:42:27 +0000

    nightly commit

---
+++ modules/system/virtualisation/default.nix
@@ -0,0 +1,33 @@
+{ lib, config, pkgs, ... }:
+
+{
+  options = {
+    systemSettings.virtualisation = {
+      enable = lib.mkEnableOption "Enable Thunar file manager";
+    };
+  };
+
+  config = lib.mkIf config.systemSettings.virtualisation.enable {
+    environment.systemPackages = [ pkgs.looking-glass-client ];
+
+    virtualisation.libvirtd = {
+      enable = true;
+      qemu = {
+        package = pkgs.qemu_kvm;
+        swtpm.enable = true;  # TPM for Windows 11
+        verbatimConfig = ''
+          cgroup_device_acl = [
+            "/dev/kvmfr0"
+          ]
+        '';
+      };
+    };
+
+    users.users.conor.extraGroups = [ "libvirtd" ];
+
+    programs.virt-manager.enable = true;
+
+    boot.kernelModules = [ "vfio-pci" ];
+    boot.kernelParams = [ "amd_iommu=on" ];
+  };
+}
