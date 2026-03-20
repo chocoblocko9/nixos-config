2b1ae6184f0a2de6b8ad6ee9050adcbbfb4a40bc
Conor <conorboyle07@protonmail.com>
2026-03-20 12:31:20 +0000

    Added context

--- modules/hjem/hyprland/default.nix
+++ modules/hjem/hyprland/default.nix
@@ -34,23 +34,19 @@
 
       files = {
         ".config/hypr/hyprland".source = ./hyprland;
-        /*
-        ".config/hypr/hyprland.conf".text = ((builtins.readFile ./hyprland.conf) 
-        + (lib.optionalString (config.hjemSettings.hyprland.profile == "slip") (builtins.readFile ./slip.conf)
-        + (lib.optionalString (config.hjemSettings.hyprland.profile == "sleepless") (builtins.readFile ./sleepless.conf))));
+
+        /*  
+        Exports an environment variable to hyprland that conditionals can use to choose
+        what config options to pick. This puts all branching into my config files which
+        is nice and also means I don't have to do the silly builtins.readFile stuff.
         */
         ".config/hypr/hyprland.conf".text = ''
           ${lib.optionalString (config.networking.hostName == "slip") "env = SLIP,1"}
           ${lib.optionalString (config.networking.hostName == "sleepless") "env = SLEEPLESS,1"}
 
-          ### EXECUTE ON BOOT ###
-          exec-once=runapp soteria
-          exec-once=runapp kitty --class kitty-nvim --config ~/.files/modules/hjem/kitty/nvim.conf nvim
-          exec-once=runapp quickshell
-          exec-once=runapp lollypop
-
           source = ~/.config/hypr/hyprland/*
         '';
+
         ".config/hypr/xdph.conf".text = ''
           screencopy {
             max_fps = 60
