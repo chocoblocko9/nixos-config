469881c214a14bcce45b517f328db2965949b77e
Conor <conorbhoyle@gmail.com>
2026-04-02 17:48:34 +0100

    annoying

--- modules/hjem/hypr/hyprland/functions.lua
+++ modules/hjem/hypr/hyprland/functions.lua
@@ -1,7 +1,7 @@
 hl.on("window.active", function(w) 
   if (w.class == "vesktop") then 
-    hl.window.alter_zorder({ window = "class:firefox", mode = "bottom" })()
+    hl.window.alter_zorder({ window = "class:vesktop", mode = "top" })()
   elseif (w.class == "firefox") then 
-    hl.window.alter_zorder({ window = "class:vesktop", mode = "bottom" })()
+    hl.window.alter_zorder({ window = "class:firefox", mode = "top" })()
   end
 end)
