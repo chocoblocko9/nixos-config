f2f17291f374fe9e4761fe78854f1a3db054b271
Conor <conorboyle07@protonmail.com>
2026-07-31 21:15:02 +0100

    whitespace

--- modules/hjem/fuzzel/default.nix
+++ modules/hjem/fuzzel/default.nix
@@ -8,45 +8,43 @@
   };
 
   config = lib.mkIf config.hjemSettings.fuzzel.enable {
-
-
-  hjem.users.conor = {
-    packages = [
-      pkgs.fuzzel
-    ];
-    
-    files.".config/fuzzel/fuzzel.ini" = {
-      generator = lib.generators.toINI {};
+    hjem.users.conor = {
+      packages = [
+        pkgs.fuzzel
+      ];
       
-      value = {
-        border.width = 3;
+      files.".config/fuzzel/fuzzel.ini" = {
+        generator = lib.generators.toINI {};
+        
+        value = {
+          border.width = 3;
 
-        # RRGGBBAA
-        colors = {
-          background = "011e25cc";
-          border = "0f5570dd";
-          counter = "eee8d5ff"; 
-          input = "cac6b8ff";
-          match = "cecb00ff";
-          placeholder = "657b83ff";
-          prompt = "cac6b8ff";
-          selection = "586e75ff";
-          selection-match = "cecb00ff";
-          selection-text = "cac6b8ff";
-          text = "cac6b8ff";
-        };
+          # RRGGBBAA
+          colors = {
+            background = "011e25cc";
+            border = "0f5570dd";
+            counter = "eee8d5ff"; 
+            input = "cac6b8ff";
+            match = "cecb00ff";
+            placeholder = "657b83ff";
+            prompt = "cac6b8ff";
+            selection = "586e75ff";
+            selection-match = "cecb00ff";
+            selection-text = "cac6b8ff";
+            text = "cac6b8ff";
+          };
 
-        main = {
-          font = "DejaVu Sans:size=16";
-          icon-theme = "Numix";
-          use-bold = true;
-          terminal = "kitty";
-          width = 60;
-          anchor = "top";
-          y-margin = 8;
+          main = {
+            font = "DejaVu Sans:size=16";
+            icon-theme = "Numix";
+            use-bold = true;
+            terminal = "kitty";
+            width = 60;
+            anchor = "top";
+            y-margin = 8;
+          };
         };
       };
     };
   };
-};
 }
